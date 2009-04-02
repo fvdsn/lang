c9d1b11a7c47aa9bf628ef94d2087acd4152ef59
Frédéric van der Essen <fred@pc-fred.mshome>
2009-04-02 15:39:50 +0200

    Début des tests.

--- src/happy/parser/bnf/Loader.java
+++ src/happy/parser/bnf/Loader.java
@@ -7,21 +7,23 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import happy.checker.*;
 
 public class Loader {
 	
 	List<Rule> rules;
 
 	public static void main(String[] args) throws Exception {
-		Loader load = new Loader("grammar_wp.bnf");
-	
+		Loader load = new Loader("grammar_simple.bnf");
 		for(Rule r : load.getRules()) {
 			System.out.println(r);
 			System.out.println();
 		}
+		List<Term> l = CheckPrecedence.getAllTerm(load.getRules());
+		for(Term t:l){
+			System.out.println(t.toString());
+		}
 	}
-	
-	
 	public Loader(String file) throws FileNotFoundException, IOException {
 		rules = new ArrayList<Rule>();
 		File f = new File(file);
