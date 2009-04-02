4ab7ca9eee14fc54a2584e21935f4da9aaddee25
Thibault Francois <mythrys@mythrys-village.(none)>
2009-04-02 20:38:07 +0200

    load

---
+++ src/list/all/Token.java
@@ -0,0 +1,14 @@
+package list.all;
+
+public class Token {
+	
+	public String name;
+	public String type;
+	
+	
+	public Token(String name,String type){
+		this.name = name;
+		this.type = type;
+	}
+
+}
