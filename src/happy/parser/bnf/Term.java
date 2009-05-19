2a487ce7932c5af7507e25432b8cc5c166ff9fad
mythrys <mythrys@mythrys-laptop.(none)>
2009-05-19 02:54:45 +0200

    test

--- src/happy/parser/bnf/Term.java
+++ src/happy/parser/bnf/Term.java
@@ -8,14 +8,41 @@ public interface Term {
 	 * @return true if the term is a terminal term
 	 */
 	public boolean isTerminal();
+	
+	/**
+	 * 
+	 * @return Le type du term pour l'analyseur syntaxique.
+	 */
 	public String getType();
+	
+	/**
+	 * 
+	 * @return la valeur du term, c'est à dire la chaine de caractère
+	 * parsée par l'analyseur lexical.
+	 * Si le term n'est pas terminal, il n'a pas de valeur
+	 */
 	public String getValue();
+	
+	/**
+	 * Modifie la valeur du term
+	 * @param v la nouvelle valeur du term
+	 * @return this
+	 */
 	public Term setValue(String v);
-	int	   getLine();
-	void   setLine(int line);
-	int		getCollumn();
-	void	setCollumn(int col);
+	
+	/**
+	 * Renvoie la liste des enfants du terme, cette liste n'est pas vide que
+	 * si le term n'es pas terminal
+	 * @return
+	 */
 	public List<Term> getChildList();
+	
+	/**
+	 * Imprime l'arbre qui est représenté par le term
+	 * Indent définit le niveau d'indentation, lorsqu'on imprime
+	 * l'arbre entier le point de départ est 0
+	 * @param indent
+	 */
 	void	printTree(int indent);
 	@Override
 	public int hashCode();
